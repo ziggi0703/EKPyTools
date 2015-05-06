0394f02f62c6d23d78cec07ac9130968eb79a552
Michael Ziegler <Michael.Ziegler2@kit.edu>
2015-05-06 11:03:10 +0200

    Add colors

--- ekpytools/colors/kit.py
+++ ekpytools/colors/kit.py
@@ -11,3 +11,22 @@ brown = rgb_to_hex_rep(167, 130, 46)
 red = rgb_to_hex_rep(162, 34, 35)
 purple = rgb_to_hex_rep(163, 16, 124)
 cyan_blue = rgb_to_hex_rep(35, 161, 224)
+
+
+def get_palette(n_colors=5):
+    """
+    Load the kit color palette
+
+    :param n_colors: number of different colors int hte palette
+    :type n_colors: int
+
+    :return: kit standard colors
+    :rtype: list
+    """
+    colors = [green, pea_green, blue, cyan_blue, purple, brown, red, orange, yellow]
+
+    if n_colors > len(colors):
+        print 'Only {} different colors in list. All are returned.'
+        return colors
+
+    return colors[n_colors:]
