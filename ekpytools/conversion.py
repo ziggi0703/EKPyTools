54e13581de377960c2dfa2bca8a97852b7a4977a
Michael Ziegler <Michael.Ziegler2@kit.edu>
2015-05-06 11:03:50 +0200

    add function convert_data_frame_to_ttree

--- ekpytools/conversion.py
+++ ekpytools/conversion.py
@@ -2,6 +2,10 @@
 __author__ = 'Michael Ziegler'
 
 
+from root_numpy import tree2array, arry2tree
+import pandas as pd
+
+
 def convert_ttree_to_data_frame(tree, variables=None, **kwargs):
     """
     Convert a ROOT.TTree to a pandas.DataFrame using the root_numpy.tree2array
@@ -16,9 +20,6 @@ def convert_ttree_to_data_frame(tree, variables=None, **kwargs):
     :return: Converted DataFrame
     :rtype: pandas.DataFrame
     """
-    from root_numpy import tree2array
-    import pandas as pd
-
     if variables is not None:
         array = tree2array(tree, branches=variables)
     else:
@@ -27,3 +28,22 @@ def convert_ttree_to_data_frame(tree, variables=None, **kwargs):
     data_frame = pd.DataFrame(array)
 
     return data_frame
+
+
+def convert_data_frame_to_ttree(data_frame, columns=None, **kwargs):
+    """
+    Convert a pandas.DataFrame to a ROOT.TTree
+
+    :param data_frame: data that is written to a TTree
+    :type data_frame: pandas.DataFrame
+
+    :param columns: columns that are written to the TTree. If None (default), all columns in data_frame
+        are written to the TTree
+    :type columns: list, None
+
+    :param kwargs: More keyword arguments
+    :type kwargs:
+
+    :return: converted TTree
+    :rtype: ROOT.TTree
+    """
