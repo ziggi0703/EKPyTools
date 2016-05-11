1e131e8282774383c67efc5212645c7cf0b21245
Michael Ziegler <Michael.Ziegler2@kit.edu>
2016-05-11 16:47:55 +0200

    add function weighted_std and patch pandas.Series

--- ekpytools/__init__.py
+++ ekpytools/__init__.py
@@ -3,7 +3,8 @@ __author__ = 'Michael Ziegler'
 
 __all__ = ['datahandling', 'conversion', 'plotting']
 
-from .statistics import weighted_mean
+from .statistics import weighted_mean, weighted_std
 from pandas import Series
 
 Series.weighted_mean = weighted_mean
+Series.weighted_std = weighted_std
--- ekpytools/statistics.py
+++ ekpytools/statistics.py
@@ -24,3 +24,33 @@ def weighted_mean(series, weights=None):
         raise ValueError('series and weights must have same length.')
 
     return np.sum(weights * series) / np.sum(weights)
+
+
+def weighted_std(series, weights=None):
+    """
+    Calculate the weighted standard deviation of a series
+
+    :param series: calculate std of this series
+    :type series: pandas.Series
+
+    :param weights: (optional) use these weights
+    :type weights: pandas.Series
+
+    :return: weighted std
+    :rtype: float
+    """
+    if weights is None:
+        return series.std()
+
+    if len(weights) != len(series):
+        raise ValueError('series and weights must have same length.')
+
+    s0 = np.sum(weights)
+
+    if s0 == 0:
+        return 0
+
+    s1 = np.sum(weights * series)
+    s2 = np.sum(weights * series * series)
+
+    return np.sqrt((s2 / s0 - s1**2/s0**2))
