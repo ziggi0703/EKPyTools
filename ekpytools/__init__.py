571f4bef1e66b12e73fb7ad5f770e04c02daa79e
Michael Ziegler <Michael.Ziegler2@kit.edu>
2016-05-11 15:07:49 +0200

    Add function weighted_mean. Patch pandas.Series.

--- ekpytools/__init__.py
+++ ekpytools/__init__.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python 
 __author__ = 'Michael Ziegler'
+
+__all__ = ['datahandling', 'conversion', 'plotting']
+
+from .statistics import weighted_mean
+from pandas import Series
+
+Series.weighted_mean = weighted_mean
--- ekpytools/statistics.py
+++ ekpytools/statistics.py
@@ -1,1 +1,26 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
+from __future__ import division, absolute_import, print_function
+import pandas as pd
+import numpy as np
+
+
+def weighted_mean(series, weights=None):
+    """
+    Calculate the weighted mean of a series.
+
+    :param series: calculate mean of this series
+    :type series: pandas.Series
+
+    :param weights: (optional) series with weights.
+    :type weights: pandas.Series
+
+    :return: mean of the series
+    :rtype: float
+    """
+    if weights is None:
+        return series.mean()
+
+    if len(weights) != len(series):
+        raise ValueError('series and weights must have same length.')
+
+    return np.sum(weights * series) / np.sum(weights)
